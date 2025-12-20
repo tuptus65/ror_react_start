897883b795e1bcccc1bdd4f7b997ab257948b6c8
tuptus65 <tuptus@turox.org.pl>
2025-12-20 11:08:10 +0000

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// Plik stworzony tylko dla kompatybilności z IDE (RubyMine)
+module.exports = {
+    content: [
+        './app/views/**/*.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js'
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
